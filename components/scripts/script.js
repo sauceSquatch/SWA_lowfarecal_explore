18443e56200dfeff28a5e120bd4066d1cc6b9e85
Jamie Naioti <naioti@gmail.com>
2015-12-14 17:29:59 -0600

    added a fade to the headers, adjusted the header width and modified timing a little

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -10,6 +10,9 @@ var departure = $(".departure"),
     departCal = $(".cal_depart"),
     departDay = $(".depart_day"),
     departCalFade = $(".fade_overlay"),
+    headerFadeDepart = $(".header_fade", ".departure"),
+    headerFadeArrive = $(".header_fade", ".arrival"),
+    baseSpeed = 0.45,
     calIsCollapsed = false;
 
 
@@ -17,6 +20,8 @@ var departure = $(".departure"),
 initBuild = function() {
   TweenLite.set(departCalFade, {alpha:0});
   TweenLite.set(departDay, {alpha:0});
+  TweenLite.set(headerFadeDepart, {alpha:0});
+  TweenLite.set(headerFadeArrive, {alpha:0.5});
   addListeners();
 }
 
@@ -31,9 +36,13 @@ collapseCal = function(evt) {
   if(!calIsCollapsed) {
     calIsCollapsed = true;
     TweenLite.to(departDay, 0.15, {alpha:1});
-    TweenLite.to(departCal, 0.45, {y:-147, ease:Power4.easeInOut});
-    TweenLite.to(departCalFade, 0.45, {alpha:0.5});
-    TweenLite.to(departCalContainer, 0.45, {height:107, ease:Power4.easeInOut});
+    TweenLite.to(departCal, baseSpeed, {y:-147, ease:Power4.easeInOut});
+    TweenLite.to(departCalFade, baseSpeed, {alpha:0.5});
+    TweenLite.to(departCalContainer, baseSpeed, {height:105, ease:Power4.easeInOut});
+    //headers
+    TweenLite.to(headerFadeDepart, 0.1, {alpha:0.5});
+    TweenLite.to(headerFadeArrive, 0.1, {alpha:0});
+
   } else {
     expandCal();
   }
@@ -43,9 +52,12 @@ expandCal = function(evt) {
   if(calIsCollapsed) {
     calIsCollapsed = false;
     // TweenLite.to(departDay, 0.15, {alpha:1}); // the day should stay selected
-    TweenLite.to(departCal, 0.45, {y:0, ease:Power4.easeInOut});
-    TweenLite.to(departCalFade, 0.45, {alpha:0});
-    TweenLite.to(departCalContainer, 0.45, {height:607, ease:Power4.easeInOut});
+    TweenLite.to(departCal, baseSpeed, {y:0, ease:Power4.easeInOut});
+    TweenLite.to(departCalFade, baseSpeed, {alpha:0});
+    TweenLite.to(departCalContainer, baseSpeed, {height:607, ease:Power4.easeInOut});
+    //headers
+    TweenLite.to(headerFadeDepart, 0.1, {alpha:0});
+    TweenLite.to(headerFadeArrive, 0.1, {alpha:0.5});
   } else {
     // expandCal();
   }
