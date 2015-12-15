95e8846776436a98313aaa08104f28cc9f5f1829
Jamie Naioti <naioti@gmail.com>
2015-12-15 16:21:53 -0600

    added the fair footer animate up from the bottom when they select the depart date, added a clickable return date, Nov. 17th

--- js/script.js
+++ js/script.js
@@ -13780,8 +13780,14 @@ var departure = $(".departure"),
     departCal = $(".cal_depart"),
     departDay = $(".depart_day"),
     departCalFade = $(".fade_overlay"),
+    fareOverlay = $(".current_fare"),
     headerFadeDepart = $(".header_fade", ".departure"),
     headerFadeArrive = $(".header_fade", ".arrival"),
+    fareStart = $("#fare_start"),
+    fareEnd = $("#fare_end"),
+    arrivalCal3 = $("#arrival_cal3"),
+    arrivalCal2 = $("#arrival_cal2"),
+    arriveDay = $("#return_day"),
     baseSpeed = 0.45,
     calIsCollapsed = false;
 
@@ -13792,6 +13798,10 @@ initBuild = function() {
   TweenLite.set(departDay, {alpha:0});
   TweenLite.set(headerFadeDepart, {alpha:0});
   TweenLite.set(headerFadeArrive, {alpha:0.5});
+  TweenLite.set(fareEnd, {alpha:0});
+  TweenLite.set(arrivalCal3, {alpha:0});
+  TweenLite.set(arrivalCal2, {alpha:0});
+  TweenLite.set(arriveDay, {alpha:0});
   addListeners();
 }
 
@@ -13800,6 +13810,7 @@ addListeners = function() {
   console.log("departDay: ", departDay);
   departDay.on('click', collapseCal);
   departCalFade.on('click', collapseCal);
+  arriveDay.on('click', selectReturnDay);
 }
 
 collapseCal = function(evt) {
@@ -13808,10 +13819,15 @@ collapseCal = function(evt) {
     TweenLite.to(departDay, 0.15, {alpha:1});
     TweenLite.to(departCal, baseSpeed, {y:-147, ease:Power4.easeInOut});
     TweenLite.to(departCalFade, baseSpeed, {alpha:0.5});
-    TweenLite.to(departCalContainer, baseSpeed, {height:105, ease:Power4.easeInOut});
+    TweenLite.to(departCalContainer, baseSpeed, {height:107, ease:Power4.easeInOut});
     //headers
     TweenLite.to(headerFadeDepart, 0.1, {alpha:0.5});
     TweenLite.to(headerFadeArrive, 0.1, {alpha:0});
+    // fare 
+    TweenLite.to(fareOverlay, 0.35, {y:-95, ease:Power4.easeInOut});
+    // change calendar
+    TweenLite.to(arrivalCal2, 0.35, {alpha:1, delay:baseSpeed});
+
 
   } else {
     expandCal();
@@ -13830,6 +13846,13 @@ expandCal = function(evt) {
     TweenLite.to(headerFadeArrive, 0.1, {alpha:0.5});
   } else {
     // expandCal();
+  }
+}
+
+selectReturnDay = function(evt) {
+  if(calIsCollapsed) {
+    TweenLite.to(arriveDay, 0.25, {alpha:1});
+    TweenLite.to(arrivalCal3, baseSpeed, {alpha:1});
   }
 }
 
